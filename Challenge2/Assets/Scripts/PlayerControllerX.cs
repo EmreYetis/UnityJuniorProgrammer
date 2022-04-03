2fa3b9287d8b98f5948a45042d973a3cc3054a6a
EmreYetis <emreyetis955@gmail.com>
2022-04-03 13:23:35 +0300

    Challenge2

---
+++ Challenge2/Assets/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    // Saniyede 2 tane
+    public float timeBetweenShots = 0.5f;
+    private float timestamp;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Time.time>= timestamp && (Input.GetKeyDown(KeyCode.Space)))
+        {
+           
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            timestamp = Time.time + timeBetweenShots;
+        }
+    }
+}
